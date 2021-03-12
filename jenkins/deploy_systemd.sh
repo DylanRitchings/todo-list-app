7a9eaf2cd2eb02b38bb92f76e15e1717af4b7ba5
DylanRitchings <43646210+DylanRitchings@users.noreply.github.com>
2021-03-12 15:06:07 +0000

    Update deploy_systemd.sh

--- jenkins/deploy_systemd.sh
+++ jenkins/deploy_systemd.sh
@@ -22,10 +22,8 @@ Description=Todo List
 # Attach the user either to jenkins or (preferably) 
 # dedicated service user, e.g. pythonadm
 # ----------------------------------
-DATABASE_URI = "34.105.147.232"
+DATABASE_URI = "sqlite:///data.db"
 SECRET_KEY = "hjghdjkfhgkjdfhgkjdhfg"
-MYSQL_USER='root'
-MYSQL_PASSWORD='root'
 User=pythonadm
 
 sudo bash jenkins/setup.sh
